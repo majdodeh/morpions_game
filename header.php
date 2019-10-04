65f2011c5fe80954f7a8290a2a925bbc7ce9899a
Majd Odeh <majdodeh199@gmail.com>
2019-10-04 19:48:54 +0200

    added files

---
+++ header.php
@@ -0,0 +1,38 @@
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-light nav-shadow">
+            <div class="container">
+                <a class="navbar-brand" href="index.php"><img src="img/logo.png" width="50%" height="50%" class=""/></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#project_menu" aria-controls="project_menu" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="project_menu">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item padding-h-10">
+                            <a class="nav-link" href="index.php"><strong>Accueil</strong></span></a>
+                        </li>
+                        <li class="nav-item padding-h-10">
+                            <a class="nav-link" href="index.php?page=create_team.php"><strong>Créer une équipe</strong></a>
+                        </li>
+                        <li class="nav-item padding-h-10">
+                            <a class="nav-link" href="index.php?page=create_team.php"><strong>Jouer</strong></a>
+                        </li>
+    <!--                    <li class="nav-item">-->
+    <!--                        <a class="nav-link disabled" href="#">Disabled</a>-->
+    <!--                    </li>-->
+    <!--                    <li class="nav-item dropdown">-->
+    <!--                        <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown07" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>-->
+    <!--                        <div class="dropdown-menu" aria-labelledby="dropdown07">-->
+    <!--                            <a class="dropdown-item" href="#">Action</a>-->
+    <!--                            <a class="dropdown-item" href="#">Another action</a>-->
+    <!--                            <a class="dropdown-item" href="#">Something else here</a>-->
+    <!--                        </div>-->
+    <!--                    </li>-->
+                    </ul>
+    <!--                <form class="form-inline my-2 my-md-0">-->
+    <!--                    <input class="form-control" type="text" placeholder="Search" aria-label="Search">-->
+    <!--                </form>-->
+                </div>
+            </div>
+        </nav>
+    </header>
